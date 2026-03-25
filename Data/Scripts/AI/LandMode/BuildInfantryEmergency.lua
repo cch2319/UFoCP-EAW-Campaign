f8494f20315633cf0420941877d4da28b1325e80
James Suzuki <20891893+AlyMar1994@users.noreply.github.com>
2026-03-25 01:22:40 -0400

    Create BuildInfantryEmergency.lua

---
+++ Data/Scripts/AI/LandMode/BuildInfantryEmergency.lua
@@ -0,0 +1,70 @@
+-- $Id: //depot/Projects/StarWars_Steam/FOC/Run/Data/Scripts/AI/LandMode/BuildInfantryEmergency.lua#1 $
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+-- (C) Petroglyph Games, Inc.
+--
+--
+--  *****           **                          *                   *
+--  *   **          *                           *                   *
+--  *    *          *                           *                   *
+--  *    *          *     *                 *   *          *        *
+--  *   *     *** ******  * **  ****      ***   * *      * *****    * ***
+--  *  **    *  *   *     **   *   **   **  *   *  *    * **   **   **   *
+--  ***     *****   *     *   *     *  *    *   *  *   **  *    *   *    *
+--  *       *       *     *   *     *  *    *   *   *  *   *    *   *    *
+--  *       *       *     *   *     *  *    *   *   * **   *   *    *    *
+--  *       **       *    *   **   *   **   *   *    **    *  *     *   *
+-- **        ****     **  *    ****     *****   *    **    ***      *   *
+--                                          *        *     *
+--                                          *        *     *
+--                                          *       *      *
+--                                      *  *        *      *
+--                                      ****       *       *
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+-- C O N F I D E N T I A L   S O U R C E   C O D E -- D O   N O T   D I S T R I B U T E
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+--              $File: //depot/Projects/StarWars_Steam/FOC/Run/Data/Scripts/AI/LandMode/BuildInfantryEmergency.lua $
+--
+--    Original Author: James Yarrow
+--
+--            $Author: Brian_Hayes $
+--
+--            $Change: 637819 $
+--
+--          $DateTime: 2017/03/22 10:16:16 $
+--
+--          $Revision: #1 $
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+
+require("pgevents")
+
+
+function Definitions()
+	
+	Category = "Tactical_Multiplayer_Build_Infantry_Emergency"
+	IgnoreTarget = true 
+	TaskForce = {
+	{
+		"ReserveForce",
+		"Infantry | LandHero = 1",
+		"-Vornskr_Wolf",
+		"-Veers_AT_AT_Walker",
+		"-Gargantuan_Battle_Platform",
+		"-Tactical_R2_3PO_Team"
+	}
+	}
+	AllowFreeStoreUnits = false
+
+end
+
+function ReserveForce_Thread()
+			
+	BlockOnCommand(ReserveForce.Produce_Force())
+	ReserveForce.Set_Plan_Result(true)
+	ReserveForce.Set_As_Goal_System_Removable(false)
+	Sleep(15)		
+	ScriptExit()
+end
