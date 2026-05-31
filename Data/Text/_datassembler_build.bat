d1bfa07b83110e9fc9ca2d88325134af01085ebc
James Suzuki <20891893+AlyMar1994@users.noreply.github.com>
2026-05-31 17:11:14 -0400

    Create _datassembler_build.bat

---
+++ Data/Text/_datassembler_build.bat
@@ -0,0 +1,7 @@
+_datassembler.exe /b MasterTextFile_ENGLISH.txt MasterTextFile_ENGLISH.dat
+_datassembler.exe /b MasterTextFile_FRENCH.txt MasterTextFile_FRENCH.dat
+_datassembler.exe /b MasterTextFile_GERMAN.txt MasterTextFile_GERMAN.dat
+_datassembler.exe /b MasterTextFile_ITALIAN.txt MasterTextFile_ITALIAN.dat
+_datassembler.exe /b MasterTextFile_POLISH.txt MasterTextFile_POLISH.dat
+_datassembler.exe /b MasterTextFile_RUSSIAN.txt MasterTextFile_RUSSIAN.dat
+_datassembler.exe /b MasterTextFile_SPANISH.txt MasterTextFile_SPANISH.dat
