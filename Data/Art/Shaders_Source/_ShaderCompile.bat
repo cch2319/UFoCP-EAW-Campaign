3fd95d5baf66b30e4d6decddc867b31b1b0ed36c
James Suzuki <20891893+AlyMar1994@users.noreply.github.com>
2026-06-02 17:08:08 -0400

    chore: update ShaderCompile

--- Data/Art/Shaders_Source/_ShaderCompile.bat
+++ Data/Art/Shaders_Source/_ShaderCompile.bat
@@ -1,15 +1,43 @@
-REM Created by [RA] Barlex (lord_poncho on Discord)
-title Shader Compiler
+:: Created by Barlex (Discord: lord_poncho)
+:: Modified by jsuzu (Discord: AlyMar1994)
+:: -----
+:: Originally asked for directory and which individual
+:: file to convert - now asks for that or a batch
+:: conversion of all files this bat can see.
+@ECHO OFF
+TITLE Shader Compiler v2.0.0
 
-REM Sets current drive, and file path. Change to whatever you use
-D:
-cd D:\_Steam\steamapps\common\Star Wars Empire at War\corruption\Mods\UFCP\Data\Art\Shaders_Source
+:LOOP
+	CLS
+	ECHO 1 - Convert a Single Shader
+	ECHO 2 - Convert Batches of Shaders
+	ECHO .
+	ECHO 3 - Exit
+	ECHO .
+	CHOICE /c:123
 
-@echo off
-echo Enter Shader Name: 
-set /p sourceCode=
-@echo on
+	IF ERRORLEVEL 255 GOTO DONE
+	IF ERRORLEVEL 3 GOTO DONE
+	IF ERRORLEVEL 2 GOTO BATCH
+	IF ERRORLEVEL 1 GOTO SINGLE
+	IF ERRORLEVEL 0 GOTO DONE
 
-fxc.exe /Tfx_2_0 /LD /Fo"%sourceCode%.fxo" "%sourceCode%.fx"
 
-pause
+:SINGLE
+	set /p "SHADER=Enter Shader Name: "
+
+	_fxc.exe /T fx_2_0 /LD /Fo "%SHADER%.fxo" "%SHADER%.fx"
+
+	PAUSE
+	GOTO LOOP
+
+
+:BATCH
+	for %%i in (*.fx) do _fxc.exe /Tfx_2_0 /LD /Fo"%%~ni.fxo" "%%i"
+
+	PAUSE
+	GOTO LOOP
+
+
+:DONE
+	EXIT
